96b979750458df2737df3477e70a3b4e51d383af
sandhya2000 <53313956+sandhya2000@users.noreply.github.com>
2025-02-22 17:56:17 +0530

    section 9.1

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -2,27 +2,70 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Product } from '../../shared/models/product';
 import { ShopService } from '../../core/services/shop.service';
 import { ProductItemComponent } from "./product-item/product-item.component";
+import { MatDialog } from '@angular/material/dialog';
+import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
   imports: [
-    ProductItemComponent
+    ProductItemComponent,
+    MatButton,
+    MatIcon
 ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
 export class ShopComponent implements OnInit {
   private shopService=inject(ShopService);
+  private dialogService=inject(MatDialog)
   
   products: Product[] =[];
+  selectedBrands: string[] =[];
+  selectedTypes: string[]=[];
 
 
   ngOnInit(): void {
+    this.initializeShop();
+  }
+
+  initializeShop(){
+    this.shopService.getBrands();
+    this.shopService.getTypes();
     this.shopService.getProducts().subscribe({
       next: response=>this.products=response.data,
       error: error=>console.log(error),
       
-    })
+    });
+  }
+
+  openFiltersDialog(){
+    const dialogRef= this.dialogService.open(FiltersDialogComponent,{
+      minWidth: '500px',
+      data:{
+         selectedBrands: this.selectedBrands,
+         selectedTypes: this.selectedTypes
+      }
+    });
+   
+  dialogRef.afterClosed().subscribe({
+    next: result =>{
+      if(result){
+        console.log(result);
+        this.selectedBrands= result.selectedBrands;
+        this.selectedTypes= result.selectedTypes;
+        this.shopService.getProducts(this.selectedBrands,this.selectedTypes).subscribe(
+          {
+            next: response => this.products= response.data,
+            error: error=>console.log(error)
+            
+          }
+        )
+      }
+    }
+  })
+    
   }
 }
